aa8e3f30ff76d5912499d6a93c5d67dfeee5980e
Juan Pablo Useche <jpuseche@uninorte.edu.co>
2022-06-03 09:22:48 -0500

    added welcome and fixed logic for buy and sell

--- stocksimulator/settings.py
+++ stocksimulator/settings.py
@@ -16,7 +16,7 @@ import os
 from numpy import True_
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(_file_).resolve().parent.parent
 PROJECT_DIR = os.path.join(BASE_DIR, "stocksimulator")
 
 
